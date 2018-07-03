c0de4b27e2654a2dec97c8c06a1cf5ce13c09bd7
PC\admin <o.mayen0308@gmail.com>
2018-07-03 08:24:13 -0600

    Agregacion de paquetes y clase conexion

---
+++ src/com/conexion/Conexion.java
@@ -0,0 +1,66 @@
+
+package com.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *Nombre de la clase: Conexion
+ * Fecha: 24-06-2018
+ * Version:1.0
+ * CopyRight:ITCA-FEDEPADE
+ * @author: Oscar Mayen
+ */
+public class Conexion 
+{
+    private Connection con;
+
+    
+    //Metodo GET y SET
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    
+    //METODO CONECTAR
+    
+    public void conectar() throws Exception
+    {
+        try 
+        {
+           Class.forName("com.mysql.jdbc.Driver"); 
+           con=DriverManager.getConnection("jdbc:mysql://localhost:3306/estudianteprocedimientos?user=root&password=");
+          
+        } 
+        
+        catch (Exception e) 
+        {
+            JOptionPane.showMessageDialog(null, "Error al conectar" + e.toString());
+        }
+        
+    }
+    
+    //METODO DESCONECTAR
+    public  void desconectar() throws Exception
+    {
+        try 
+        {
+            if(con!=null)
+            {
+                if(con.isClosed()==false)
+                {
+                    con.close();
+                }
+            
+            }    
+        } 
+        catch (Exception e) 
+        {
+            throw e;
+        }
+    }
+}
